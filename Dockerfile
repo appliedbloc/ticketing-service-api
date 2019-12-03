c979c87f53ec17f6517f8827a6778af24970686d
seanbc86 <57074516+seanbc86@users.noreply.github.com>
2019-12-02 22:01:35 -0500

    Dockerfile has been added and tested

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3.7
+
+COPY . /app
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+RUN python setup.py develop
+
+EXPOSE 8888
+
+ENTRYPOINT python ticketing_service/app.py
+#CMD [ "python", "ticketing_service/app.py" ]
--- ticketing_service/settings.py
+++ ticketing_service/settings.py
@@ -1,12 +1,12 @@
 # Flask settings
-FLASK_SERVER_NAME = 'localhost:8888'
+FLASK_SERVER_NAME = '0.0.0.0:8888'
 FLASK_DEBUG = True  # Do not use debug mode in production
 
 # Flask-Restplus settings
 RESTPLUS_SWAGGER_UI_DOC_EXPANSION = 'list'
 RESTPLUS_VALIDATE = True
 RESTPLUS_MASK_SWAGGER = False
-RESTPLUS_ERROR_404_HELP = False
+RESTPLUS_ERROR_404_HELP = True
 
 # SQLAlchemy settings
 SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
