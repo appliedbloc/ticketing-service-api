0e767c91bea559e07e01f24f3ed19ecbd34c2415
yev.kravets@appliedbloc.com <yevgenkravets@Yevgens-MacBook-Pro.local>
2019-12-05 11:08:39 -0500

    Settings change to connect to AWS-hosted PostgreSQL database

--- ticketing_service/settings.py
+++ ticketing_service/settings.py
@@ -1,13 +1,20 @@
-# Flask settings
-FLASK_SERVER_NAME = 'localhost:8888'
-FLASK_DEBUG = True  # Do not use debug mode in production
+#Flask settings 
+FLASK_SERVER_NAME = '0.0.0.0:8888'
+FLASK_DEBUG = True  # Do not use debug mode in production  
 
-# Flask-Restplus settings
+# Flask-Restplus settings 
 RESTPLUS_SWAGGER_UI_DOC_EXPANSION = 'list'
 RESTPLUS_VALIDATE = True
 RESTPLUS_MASK_SWAGGER = False
 RESTPLUS_ERROR_404_HELP = False
 
-# SQLAlchemy settings
-SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
+# Postgres 
+POSTGRES_URL = 'ticketdb.czt7kawu5hkq.us-east-1.rds.amazonaws.com:5432'
+POSTGRES_USER = 'BAMF'
+POSTGRES_PW = 'Appli3dbl0c!!'
+POSTGRES_DB = 'ticketapi'
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+
+# SQLAlchemy settings 
+SQLALCHEMY_DATABASE_URI = DB_URL
 SQLALCHEMY_TRACK_MODIFICATIONS = False
